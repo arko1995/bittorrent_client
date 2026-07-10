540c397859e7ef8dc4138590087ef0ce6db36916
arko1995 <shahriarsohelarko@gmail.com>
2026-07-10 09:46:40 +0600

    implementing tracker

--- index.js
+++ index.js
@@ -1,25 +1,13 @@
 import fs from "node:fs";
 import bencode from "bencode";
 import dgram from "node:dgram";
-
+import { getPeers } from "./tracker.js";
 import { Buffer } from "buffer";
 
 const torrent = bencode.decode(
   fs.readFileSync("lib/peter-thomas-ZIfKCrvR81I-unsplash.jpg.torrent"),
 );
 
-const tracker = Buffer.from(torrent.announce).toString("utf8");
-
-const url = new URL(tracker);
-
-const socket = dgram.createSocket("udp4");
-
-const myMsg = Buffer.from("hello", "utf8");
-
-socket.send(myMsg, 0, myMsg.length, url.port, url.hostname, (err) => {
-  console.log(err);
-});
-
-socket.on("message", (msg) => {
-  console.log("message is ", msg);
+getPeers(torrent, (peers) => {
+  console.log("list o peers ", peers);
 });
---
+++ tracker.js
@@ -0,0 +1,42 @@
+import dgram from "node:dgram";
+import { Buffer } from "node:buffer";
+import crypto from "node:crypto";
+
+export const getPeers = (torrent, callback) => {
+  const socket = dgram.createSocket("udp4");
+  const url = new URL(Buffer.from(torrent.announce)).toString("utf8");
+
+  udpSend(socket, buildConnReq(), url);
+
+  socket.on("message", (response) => {
+    if (respType(response) === "connect") {
+      const connResp = parseConnResp(response);
+
+      const announceReq = buildAnnounceReq(connResp.connectionId);
+      udpSend(socket, announceReq, url);
+    } else if (respType(response) === "announce") {
+      const announceResp = parseAnnounceResp(response);
+
+      callback(announceResp.peers);
+    }
+  });
+};
+
+function udpSend(socket, message, rawUrl, callback = () => {}) {
+  const url = new URL(rawUrl);
+  socket.send(message, 0, message.length, url.port, url.hostname, callback);
+}
+
+function respType(resp) {}
+function buildConnReq() {
+  const buf = Buffer.alloc(16);
+  buf.writeUInt32BE(0x417, 0);
+  buf.writeUInt32BE(0x27101980, 4);
+  buf.writeUInt32BE(0, 8);
+
+  crypto.randomBytes(4).copy(buf, 12);
+  return buf;
+}
+function parseConnResp() {}
+function buildAnnounceReq(connId) {}
+function parseAnnounceResp(resp) {}
