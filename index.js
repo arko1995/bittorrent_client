1d140be5c74ceffc44aa3b7e0dd70d767bd8bcff
arko1995 <shahriarsohelarko@gmail.com>
2026-07-22 16:05:52 +0600

    download

--- index.js
+++ index.js
@@ -4,10 +4,13 @@ import dgram from "node:dgram";
 import { getPeers } from "./src/tracker.js";
 import { Buffer } from "buffer";
 import * as torrentParser from "./src/torrent-parser.js";
+import * as download from "./src/download.js";
 
 const torrent = torrentParser.open(
   "lib/The Bureau XCOM Declassified - [DODI Repack].torrent",
 );
+
+download(torrent);
 
 getPeers(torrent, (peers) => {
   console.log("list o peers ", peers);
