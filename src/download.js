195124e0c049b6131f46dbb5bae0c83e9ba27b86
arko1995 <shahriarsohelarko@gmail.com>
2026-08-27 13:27:46 +0600

    added have and bitfield handler

--- src/download.js
+++ src/download.js
@@ -69,19 +69,25 @@ function unChokeHandler(socket, pieces, queue) {
   requestPiece(socket, pieces, queue);
 }
 
-function haveHandler(payload, socket, requested, queue) {
+function haveHandler(socket, pieces, payload, queue) {
   const pieceIndex = payload.readUInt32BE(0);
-  if (!requested[pieceIndex]) {
-    socket.write(message.buildRequest());
-  }
-  requested[pieceIndex] = true;
-  queue.push(pieceIndex);
-  if (queue.length === 1) {
-    requestPiece(socket, requested, queue);
-  }
+  const queueEmpty = queue.length === 0;
+  queue.queue(pieceIndex);
+  if (queueEmpty) requestPiece(socket, pieces, queue);
 }
 
-function bitfieldHandler(payload) {}
+function bitfieldHandler(socket, pieces, queue, payload) {
+  const queueEmpty = queue.length === 0;
+  payload.forEach((byte, i) => {
+    for (let j = 0; j < 8; j++) {
+      if (byte % 2) {
+        queue.queue(i * 8 + 7 - j);
+        byte = Math.floor(byte / 2);
+      }
+    }
+  });
+  if (queueEmpty) requestPiece(socket, pieces, queue);
+}
 
 function pieceHandler(payload, socket, requested, queue) {
   queue.shift();
