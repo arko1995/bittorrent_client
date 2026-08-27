992fa3cf199811703a70f64237b895eb2118ad7c
arko1995 <shahriarsohelarko@gmail.com>
2026-08-28 01:49:49 +0600

    finalizing

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
-import fs from "node:fs";
-import bencode from "bencode";
-import dgram from "node:dgram";
 import { getPeers } from "./src/tracker.js";
-import { Buffer } from "buffer";
+
 import * as torrentParser from "./src/torrent-parser.js";
 import download from "./src/download.js";
 
 const torrent = torrentParser.open(process.argv[2]);
 
-download(torrent);
+download(torrent, torrent.info.name);
 
 getPeers(torrent, (peers) => {
   console.log("list o peers ", peers);
