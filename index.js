2a9dbce9741e10dde2bdd386951759ab40250749
arko1995 <shahriarsohelarko@gmail.com>
2026-09-19 21:45:40 +0600

    finalizing

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 import * as torrentParser from "./src/torrent-parser.js";
 import download from "./src/download.js";
 
-const torrent = torrentParser.open(process.argv[2]);
+const torrentPath = process.argv[2];
 
-if (!torrent) {
+if (!torrentPath) {
   console.error("Usage: node index.js <path-to-torrent>");
   process.exit(1);
 }
+const torrent = torrentParser.open(torrentPath);
 
 download(torrent);
